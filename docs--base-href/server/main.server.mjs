b704bd889baaf3038b791aef273d6a7f0ebd690a
Ahmed Taghyan <ahmed.taghyan@egyptian-steel.com>
2024-09-24 15:05:49 +0300

    update url0

--- docs--base-href/server/main.server.mjs
+++ docs--base-href/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4NO3JP5M.mjs";import"./chunk-N2CUDZIW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-JUUNFRW4.mjs";import"./chunk-N2CUDZIW.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
